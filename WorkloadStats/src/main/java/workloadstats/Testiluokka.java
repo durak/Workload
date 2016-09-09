f0085f4d2b2535f77866832869c3ed58bebd741e
durak <itapkos@gmail.com>
2016-09-09 18:57:39 +0300

    alkujärkkäilyjä

---
+++ WorkloadStats/src/main/java/workloadstats/Testiluokka.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workloadstats;
+
+/**
+ *
+ * @author Ilkka
+ */
+public class Testiluokka {
+
+    private String aaa;
+
+    public Testiluokka(String syote) {
+        aaa = syote;
+    }
+    
+    public String getAaa() {
+        return aaa;
+    }
+}
